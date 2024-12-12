c0031f816afba16966d0a4db59ea51e349fe9790
root <root@DESKTOP-LLQEH5A.localdomain>
2024-12-12 13:36:44 +0000

    modified trash.h

--- trash/trash.h
+++ trash/trash.h
@@ -1,13 +1,13 @@
 //elf type
-#define EI_MAG0 0 // File identification
-#define EI_MAG1 1 // File identification
-#define EI_MAG2 2 // File identification
-#define EI_MAG3 3 // File identification
-#define EI_CLASS 4 // File class
-#define EI_DATA 5 // Data encoding
-#define EI_VERSION 6 // File version
-#define EI_PAD 7 // Start of padding bytes
-#define EI_NIDENT 16 // Size of e_ident[]
+#define EI_MAG0     0 // File identification
+#define EI_MAG1     1 // File identification
+#define EI_MAG2     2 // File identification
+#define EI_MAG3     3 // File identification
+#define EI_CLASS    4 // File class
+#define EI_DATA     5 // Data encoding
+#define EI_VERSION  6 // File version
+#define EI_PAD      7 // Start of padding bytes
+#define EI_NIDENT   16 // Size of e_ident[]
 
 //Type of files
 #define EI_NIDENT  16
@@ -33,6 +33,39 @@
 //e_version 
 #define EV_NONE 0 Invalid versionn
 #define EV_CURRENT 1 Current version
+
+//section header indexes
+#define SHN_UNDEF     0
+#define SHN_LORESERVE 0xff00
+#define SHN_LOPROC    0xff00
+#define SHN_HIPROC    0xff1f
+#define SHN_ABS       0xfff1
+#define SHN_COMMON    0xfff2
+#define SHN_HIRESERVE 0xffff
+
+//section type
+#define SHT_NULL     0   //it does not have an associated section
+#define SHT_PROGBITS 1 // n holds information defined by the program
+#define SHT_SYMTAB   2  // a symbol table
+#define SHT_STRTAB   3  // n holds a string table
+#define SHT_RELA     4 // holds relocation entries with explicit addends
+#define SHT_HASH     5  //holds a symbol hash table
+#define SHT_DYNAMIC  6 // n holds information for dynamic linking
+#define SHT_NOTE     7  // n holds information that marks the file in some way
+#define SHT_NOBITS   8  //occupies no space in the file but otherwise resembles SHT_PROGBITS
+#define SHT_REL      9  //holds relocation entries without explicit addends, such as type Elf32_Rel
+#define SHT_SHLIB    10 // reserved but has unspecified semantics.
+#define SHT_DYNSYM   11  // a symbol table
+#define SHT_LOPROC   0x70000000  //e reserved for processor-specific semantics.
+#define SHT_HIPROC   0x7fffffff
+#define SHT_LOUSER   0x80000000 // specifies the lower bound of the range of indexes reserved for application programs
+#define SHT_HIUSER   0xffffffff
+
+//sh_flags
+#define SHF_WRITE 0x1
+#define SHF_ALLOC 0x2
+#define SHF_EXECINSTR 0x4
+#define SHF_MASKPROC 0xf0000000
 
 typedef unsigned int Elf32_Addr;
 typedef unsigned short Elf32_Half;
@@ -76,7 +109,18 @@ typedef struct {
 }Elf32_Ehdr;
 
 /**
- * Elf32_Shdr: 
+ * Elf32_Shdr:
+ * @sh_name: specifies the name of the section
+ * @sh_type: categorizes the section's contents and semantics.
+ * @sh_flags: support 1-bit flags that describe miscellaneous attributes
+ * @sh_addr: gives the address at which the section's first byte should reside
+ * @sh_offset: gives the byte offset from the beginning of the file to the first byte in the section
+ * @sh_size: gives the section's size in bytes
+ * @sh_link: holds a section header table index link
+ * @sh_info: holds extra information, whose interpretation depends on the section type
+ * @sh_addralign: alignment of section struct
+ * @sh_entsize: gives the size in bytes of each entry in a table of fixed-size entries
+ * 
  */
 typedef struct {
     Elf32_Word sh_name;
