7262234cc1902af32a5ae0a468986c39024f74c7
root <root@DESKTOP-LLQEH5A>
2024-12-13 10:44:10 +0000

    cleaned up

--- trash/test.c
+++ trash/test.c
@@ -1,5 +1,15 @@
 #include "trash.h"
+#include <stdio.h>
 
-int main(void){
-    return (0);
+void counter() {
+    static int count = 0; // Initialized only once
+    count++;
+    printf("Count: %d\n", count);
+}
+
+int main() {
+    counter(); // Count: 1
+    counter(); // Count: 2
+    counter(); // Count: 3
+    return 0;
 }
