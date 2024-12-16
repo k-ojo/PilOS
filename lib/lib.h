848a0bdd7e427cac3aec58535ce4984f37929886
root <root@DESKTOP-LLQEH5A>
2024-12-16 15:59:50 +0000

    added avl

--- lib/lib.h
+++ lib/lib.h
@@ -6,4 +6,13 @@ typedef unsigned char uint8_t;
 
 uint32_t crc32_byte(uint8_t *, uint32_t);
 
+typedef struct _avl
+{
+    int data;
+    /* data */
+    avlbt *left, *right;
+    
+}avlbt;
+
+
 #endif
