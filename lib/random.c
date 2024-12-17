d36874872f160da9a87150a2845601c8163c7a7b
root <root@DESKTOP-LLQEH5A>
2024-12-17 16:21:51 +0000

    added random.c

--- lib/random.c
+++ lib/random.c
@@ -2,7 +2,7 @@
 
 static unsigned long int next = 1;  // NB: "unsigned long int" is assumed to be 32 bits wide
 
-int rand_simple(void)  // RAND_MAX assumed to be 32767
+int rand(void)  // RAND_MAX assumed to be 32767
 {
     next = next * 1103515245 + 12345;
     return (unsigned int) (next / 65536) % 32768;
