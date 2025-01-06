1079bd31f3760ae67306fc99d67e29e3df021dd3
K-ojo <gideonad4@gmail.com>
2025-01-06 16:57:56 +0000

    added files

--- anitOS/multiboot_header.asm
+++ anitOS/multiboot_header.asm
@@ -1,19 +1,5 @@
-; Multiboot v1 - Compliant Header for QEMU 
-; We use multiboot v1 since Qemu "-kernel" doesn't support 
-; multiboot v2
-
-; This part MUST be 4-byte aligned, so we solve that issue using 'ALIGN 4'
-ALIGN 4
-section .multiboot_header
-    ; Multiboot macros to make a few lines later more readable
-    MULTIBOOT_PAGE_ALIGN	equ 1<<0
-    MULTIBOOT_MEMORY_INFO	equ 1<<1                                         
-    MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002                                   ; magic number 
-    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO ; flags
-    MULTIBOOT_CHECKSUM	equ - (MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)  ; checksum 
-                                ; (magic number + checksum + flags should equal 0)
-
-    ; This is the GRUB Multiboot header.
-    dd MULTIBOOT_HEADER_MAGIC
-    dd MULTIBOOT_HEADER_FLAGS
-    dd MULTIBOOT_CHECKSUM
+section .multiboot
+align 4
+dd 0x1BADB002              ; Multiboot magic number
+dd 0                       ; Flags (set to 0 for basic Multiboot)
+dd -(0x1BADB002 + 0)       ; Checksum (magic + flags + checksum = 0)
