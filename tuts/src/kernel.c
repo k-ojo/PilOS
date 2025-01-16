497d067e9db16c6bf8380f59e17e62a0314cdb78
K-ojo <gideonad4@gmail.com>
2025-01-16 13:48:18 +0000

    added kernel.h

--- tuts/src/kernel.c
+++ tuts/src/kernel.c
@@ -1,6 +1,4 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
+#include "kernel.h"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -13,26 +11,15 @@
 #endif
 
 
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,   
-};
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
+static inline uint8_t vga_entry_color(vga_color fg, vga_color bg) 
 {
 	return fg | bg << 4;
 }
@@ -57,15 +44,10 @@ size_t strlen(const char* str)
     return (len);
 }
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
-
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
-
 /**
+ * terminal_iniatilize: 
+ * 
+ * Return: nothing
 **/
 void terminal_initialize(void) 
 {
