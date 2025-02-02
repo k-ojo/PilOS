db9e3b18e590063ed852483d98094eabadffa504
K-ojo <gideonad4@gmail.com>
2025-02-02 21:18:27 +0000

    added

--- trash/test.c
+++ trash/test.c
@@ -1,10 +1,6 @@
-#include "kernel.h"
+int printf(const char *format, ...);
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
-
-void test_scroll() {
-	for (size_t y = 0; y < VGA_HEIGHT + 5; y++)
-		for (size_t x = 0; x < VGA_WIDTH; x++)
-			terminal_putchar('A' + (y % 26));
+int main(){
+	char *msg = "World";
+	printf("Hello %s!\n", msg);
 }
