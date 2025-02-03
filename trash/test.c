e175d121f7688156533cd677a7ea6b8c9ece41ce
K-ojo <gideonad4@gmail.com>
2025-02-03 14:55:05 +0000

    added files

--- trash/test.c
+++ trash/test.c
@@ -1,6 +1,17 @@
-int printf(const char *format, ...);
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-	char *msg = "World";
-	printf("Hello %s!\n", msg);
+void vulnerable_function(char *input) {
+    char buffer[10];  // Small buffer
+    strcpy(buffer, input);  // No bounds checking (buffer overflow risk)
+    printf("Buffer content: %s\n", buffer);
+}
+
+int main() {
+    char long_input[100];
+    memset(long_input, 'A', 99);
+    long_input[99] = '\0';
+
+    vulnerable_function(long_input);
+    return 0;
 }
