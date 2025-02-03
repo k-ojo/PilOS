eab586dd80aabdca6a16f210eb9976bde4a4297e
K-ojo <gideonad4@gmail.com>
2025-02-03 14:31:27 +0000

    added scroll

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -38,13 +38,30 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c) {
-	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
+/**
+ * terminal_putchar: puts character to screen
+ * @c: input character on screen
+ * Return: Nothing
+ */
+void terminal_putchar(char c) 
+{
+	if (c ==  '\n'){
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+		if (++terminal_row == VGA_HEIGHT){
+			scroll();
+			terminal_row--;
+		}
+	}
+
+	else {
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT) {
+				scroll();
+				terminal_row--;
+			}
+		}
 	}
 }
 
@@ -55,4 +72,23 @@ void terminal_write(const char* data, size_t size) {
 
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
+}
+
+/**
+ * scroll- scrolls the screen down
+ * 
+ * Return: Nothing
+ */
+void scroll(void)
+{
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			const size_t nindex = (y + 1) * VGA_WIDTH + x;
+			if (y != VGA_HEIGHT - 1)
+				terminal_buffer[index] = terminal_buffer[nindex];
+			else
+				terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
 }
